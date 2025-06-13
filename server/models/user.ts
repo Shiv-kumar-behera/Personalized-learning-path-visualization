c8fed7f2e48237cf1b34fcbe8d7a2b49dde576e5
Nishita Sharma <114693685+sharmanishita@users.noreply.github.com>
2025-06-13 13:16:48 +0530

    Update user.ts

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,8 +1,17 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 interface IUser extends Document {
   username: string;
   password: string;
+  progress: number;
 };
+
+
+const UserSchema: Schema = new Schema({
+    userId: { type: String, required: true, unique: true },
+    progress: { type: Number, required: true }
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
 
 const userSchema = new mongoose.Schema<IUser>({
   username: { type: String, required: true, unique: true },
